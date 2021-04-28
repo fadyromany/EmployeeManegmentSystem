65dade7382af9d97b0af7e4cef5e5229727aaf5b
Fady Romany <fadyromany@hotmail.com>
2021-04-28 05:35:45 +0200

    Dependency injection by startub.cs

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,95 @@
+﻿using EmployeeManegmentSystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManegmentSystem.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+        public EmployeeController()
+        {
+            _employeeRepository = new MockEmployeeRepository();
+        }
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Employee/Details/5
+        public string Details(int id)
+        {
+            
+            return _employeeRepository.GetEmployee(id).Name;
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/MockEmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManegmentSystem.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList; // constructor injection
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){Id=1,Name="emp1",Email="emp1@yahho.com",Department="HR"},
+                new Employee(){Id=2,Name="emp2",Email="emp2@yahho.com",Department="IT"},
+                new Employee(){Id=3,Name="emp3",Email="emp3@yahho.com",Department="HR"},
+                new Employee(){Id=4,Name="emp4",Email="emp4@yahho.com",Department="IT"},
+                new Employee(){Id=5,Name="emp5",Email="emp5@yahho.com",Department="HR"}
+            };
+        }
+        public Employee GetEmployee(int id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
