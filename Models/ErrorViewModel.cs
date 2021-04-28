781aca3c0989b8ff43cbf66221d4d25bd62a5fdf
Fady Romany <fadyromany@hotmail.com>
2021-04-28 03:49:33 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeManegmentSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
